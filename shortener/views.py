6fd746564f88f9a4ac79776e12474fd818be8409
Ilya <ilya.kramt@gmail.com>
2022-03-26 20:51:40 +0300

    add shortener viewset

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Shortener
+from .serializers import ShortenerSerializer
+
+
+class ShortenerViewSet(viewsets.ModelViewSet):
+    queryset = Shortener.objects.all()
+    serializer_class = ShortenerSerializer
