35aa55c082fbb1f94ebaba6682c7f90cd5804f3e
Ilya <ilya.kramt@gmail.com>
2022-03-28 22:03:08 +0300

    add url redirect

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from shortener.views import ShortenerViewSet
+from shortener.views import ShortenerViewSet, redirect_url
 
 router = routers.SimpleRouter()
 router.register(r'shortener', ShortenerViewSet)
@@ -10,4 +10,5 @@ router.register(r'shortener', ShortenerViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('<slug:short_id>', redirect_url, name='redirect'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from .models import Shortener
@@ -7,3 +9,8 @@ from .serializers import ShortenerSerializer
 class ShortenerViewSet(viewsets.ModelViewSet):
     queryset = Shortener.objects.all()
     serializer_class = ShortenerSerializer
+
+
+def redirect_url(request, short_id):
+    shortener = get_object_or_404(Shortener, short_id=short_id)
+    return HttpResponseRedirect(shortener.link)
